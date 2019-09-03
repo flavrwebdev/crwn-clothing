95f11466d81e5c36432a2a623325a87db80b5223
reecechown <51628631+reecechown@users.noreply.github.com>
2019-09-04 00:14:27 +0100

    updated shpw data with new data, updated cart utils to group items together and use qty

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data.jsx';
+import SHOP_DATA from './shop.data.js';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
@@ -14,14 +14,14 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    const {collections} = this.state;
-    return (<div className='shop-page'>
-      {
-        collections.map(({ id, ...otherCollectionProps}) => (
+    const { collections } = this.state;
+    return (
+      <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
           <CollectionPreview key={id} {...otherCollectionProps} />
-        ))
-      }
-    </div>);
+        ))}
+      </div>
+    );
   }
 }
 
