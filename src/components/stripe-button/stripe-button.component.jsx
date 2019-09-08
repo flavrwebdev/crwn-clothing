7cadc7bc9d84bb5611b87332a3230b78fbe722ad
reecechown <51628631+reecechown@users.noreply.github.com>
2019-09-08 21:24:59 +0100

    updated stripe key

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_kq4Sb0QuOSRYoo768EZ9CriB00kj74o44o';
 
   const onToken = token => {
     console.log(token);
