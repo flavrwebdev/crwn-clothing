c7f31819d0263b451658963df89660fd0f8efe02
reecechown <51628631+reecechown@users.noreply.github.com>
2019-09-11 00:18:22 +0100

    separating our cra app into cliet folder, added express server to help handle backend stripe payment

---
+++ client/build/precache-manifest.707e972601965e214271dd75668bbb95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f5d438914cb3b1429b4d7269ef265b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "904ad4d9c29a6474014a",
+    "url": "/static/css/main.2e23d28f.chunk.css"
+  },
+  {
+    "revision": "87b5f18ea90edded5063",
+    "url": "/static/js/2.c7ce9b76.chunk.js"
+  },
+  {
+    "revision": "904ad4d9c29a6474014a",
+    "url": "/static/js/main.3d1a97bb.chunk.js"
+  },
+  {
+    "revision": "b721ef581ceece9d2d8b",
+    "url": "/static/js/runtime~main.43ea9386.js"
+  }
+]);
